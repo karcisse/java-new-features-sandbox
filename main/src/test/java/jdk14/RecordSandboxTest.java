86fef884e4d6bf59ebc119fc6aedc7c3d4a3d380
Karol Cissewski <Karol.Cissewski@itechart-group.com>
2023-01-22 11:08:03 +0100

    Change to JDK 15

--- main/src/test/java/jdk14/RecordSandboxTest.java
+++ main/src/test/java/jdk14/RecordSandboxTest.java
@@ -1,10 +1,11 @@
-package jdk14;
+package jdk13.jdk14;
 
 import com.itechart.jdk14.Person;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RecordSandboxTest {
 
@@ -15,5 +16,32 @@ public class RecordSandboxTest {
         assertEquals("Jhon", p.firstName());
         assertEquals("Smith", p.lastName());
         assertEquals(54, p.age());
+    }
+
+    @Test
+    public void checkCustomConstructor() {
+        Executable exec = () -> {
+            Person p = new Person("Jhon", "Smith", -54);
+        };
+
+        assertThrows(IllegalArgumentException.class, exec);
+    }
+
+    @Test
+    public void toStringEqualsHashCode() {
+        Person p1 = new Person("Jhon", "Smith", 54);
+        Person p2 = new Person("Jhon", "Doe", 54);
+        Person p3 = new Person("Jhon", "Smith", 54);
+
+        assertEquals(p1, p3);
+        assertNotEquals(p1, p2);
+        assertNotEquals(p3, p2);
+
+        assertEquals(p1.hashCode(), p3.hashCode());
+        assertNotEquals(p1.hashCode(), p2.hashCode());
+        assertNotEquals(p3.hashCode(), p2.hashCode());
+
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
